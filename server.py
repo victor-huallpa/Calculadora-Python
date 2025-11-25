369a82541c2f68be4711d8fa78ff4e3571104115
victor-huallpa <vech.600@gmail.com>
2025-11-25 15:50:56 -0500

    cambio de run

--- server.py
+++ server.py
@@ -10,4 +10,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Ejecutar en modo debug para desarrollo
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
+    # app.run(debug=True, host='0.0.0.0', port=5000)
